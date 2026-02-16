faa888ae57dff59e89bfa03626e33683c8aaa4f5
MonaTester <monatofano@gmail.com>
2026-02-16 15:34:16 +0800

    Added the Challenge Task

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,22 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
 
+app.get('/api/posts', (req, res) => {
+    const filePath = path.join(__dirname, 'data', 'posts.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Unable to read blog posts' });
+        }
+
+        res.json(JSON.parse(data));
+    });
+});
+
+
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
+});
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
